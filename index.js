783873cd80bdf15a27a4752bfd9a522d2e8c6a36
Konstantin Raev <bestander@gmail.com>
2012-12-09 19:28:54 +1300

    published to npm

--- index.js
+++ index.js
@@ -19,38 +19,57 @@ var PongGame = function (width, height, SCALE) {
     ;
 
   this.world = new b2World(
-    new b2Vec2(0, 10)    //gravity
+    new b2Vec2(0, 0)    //gravity
     ,  true                 //allow sleep
   );
 
   var fixDef = new b2FixtureDef;
   fixDef.density = 1.0;
-  fixDef.friction = 0.5;
+  fixDef.friction = 1;
   fixDef.restitution = 0.8;
 
   var bodyDef = new b2BodyDef;
 
   //create ground
   bodyDef.type = b2Body.b2_staticBody;
-
   // positions the center of the object (not upper left!)
   bodyDef.position.x = width / 2 / SCALE;
   bodyDef.position.y = height / SCALE;
-
   fixDef.shape = new b2PolygonShape;
-
   // half width, half height. eg actual height here is 1 unit
   fixDef.shape.SetAsBox((600 / SCALE) / 2, (10/SCALE) / 2);
   this.world.CreateBody(bodyDef).CreateFixture(fixDef);
 
+  // create ceiling
+  bodyDef.position.x = width / 2 / SCALE;
+  bodyDef.position.y = 0;
+  this.world.CreateBody(bodyDef).CreateFixture(fixDef);
+
+  // left wall
+  bodyDef.position.x = 0;
+  bodyDef.position.y = 0;
+  fixDef.shape.SetAsBox((10 / SCALE) / 2, height/SCALE);
+  this.world.CreateBody(bodyDef).CreateFixture(fixDef);
+
+  // right wall
+  bodyDef.position.x = width / SCALE;
+  bodyDef.position.y = 0;
+  fixDef.shape.SetAsBox((10 / SCALE) / 2, height/SCALE);
+  this.world.CreateBody(bodyDef).CreateFixture(fixDef);
+
+
+
   //create some objects
   bodyDef.type = b2Body.b2_dynamicBody;
   fixDef.shape = new b2CircleShape(
-    Math.random() + 0.1 //radius
+    0.5
   );
   bodyDef.position.x = Math.random() * 25;
   bodyDef.position.y = Math.random() * 10;
-  this.world.CreateBody(bodyDef).CreateFixture(fixDef);
+  var ball = this.world.CreateBody(bodyDef);
+  ball.CreateFixture(fixDef);
+  ball.SetLinearVelocity(new b2Vec2(5, 5));
+
 
 };
 
@@ -62,7 +81,7 @@ PongGame.prototype.update = function () {
   );
   this.world.ClearForces();
   this.loop();
-}
+};
 
 PongGame.prototype.loop = function () {
   setTimeout(this.update.bind(this), 1000 / 60);
